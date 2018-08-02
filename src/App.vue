cae1440cbccfca83922799403e3a3a76db325387
robinjongeneel23 <robinjongeneel@gmail.com>
2018-08-02 11:32:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'App',
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <div class="row mb-3">
+        <div class="col">
+          <h1>Manage your Blockchain</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a class="nav-link active" href="/">Wallet &amp; Node</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/network">Network</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/chat">Chat</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
